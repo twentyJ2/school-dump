f866537593e04597b06b8812479f5a03fee28628
Regan <rxgould03@gmail.com>
2025-01-14 11:31:48 -0800

    oops fix

--- favnumber.py
+++ favnumber.py
@@ -1,3 +1,3 @@
 name=input("Enter name: ")
 favNum=float(input("Enter favorite number: "))
-print("Hello "+name+". Your favorite number is: "+str(favNum)+"\n99 x "+str(favNum)+" = "+str(favNum*99))
+print("Hello",name+". \nYour favorite number is:",favNum,"\n99 x",favNum,"=",favNum*99)

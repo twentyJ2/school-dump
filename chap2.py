323125ad14a705a2b530a88fe9522c334fa5422e
twentyJ2 <rxgould03@gmail.com>
2025-01-16 14:02:02 -0800

    finished assignments

--- chap2.py
+++ chap2.py
@@ -1,1 +1,7 @@
-print (1,2,3,sep="canbeanything")
+itemPrice=float(input("Input the item's baseline price: "))
+totalTax=round(float(itemPrice*0.025+itemPrice*0.05),2)
+totalIP=round(totalTax+itemPrice,2)
+# A little decimal trimming with round() for the end result to look cleaner with a more realistic price.
+
+print("Price of your item:",str(itemPrice),"\nCounty sales tax: 2.5%; $"+str(itemPrice*0.025)+"\nState sales tax: 5%; $"+\
+    str(itemPrice*0.05)+"\nTotal tax: $"+str(totalTax)+"\nTotal price including taxes: $"+str(totalIP))
---
+++ generalnotes.py
@@ -0,0 +1,16 @@
+'''import turtle
+turtle.speed(1)
+turtle.forward(250)
+turtle.right(144)
+turtle.forward(250)
+turtle.right(144)
+turtle.forward(250)
+turtle.right(144)
+turtle.forward(250)
+turtle.right(144)
+turtle.forward(250)
+turtle.done()'''
+
+'''print(1,2,3,sep="canbeanything")
+print(2, end="alsowhatever")
+print(3,end="!!\n")'''

124711b5a072eacfb66317cb9a220cf6b4263b32
twentyJ2 <rxgould03@gmail.com>
2025-01-22 14:22:47 -0800

    slightly more proper code and output format

--- madLibs.py
+++ madLibs.py
@@ -21,11 +21,16 @@ numR=input("Input a number: ")
 plurNounS=input("Input a plural noun: ")
 # variable end letter dictates [n'th word entry] rather than [n'th noun] or [n'th adverb].
 # Underscore (_) represents the need to append the following letters to a used word. E.g. verb_Ing = "walking" "talking"
+# Using multiple print statements due to the previous iteration's code being difficult to read.
 
-print("A vacation is when you take a trip to some "+adjeA+" place with your "+adjeB+" family.\nUsually you go to some place that is near \
-a\\an "+nounC+" or up a\\an "+nounD+".\nA good vacation place is one where you can ride "+plurNounE+" or play "+gameF+" or go hunting for \
-"+plurNounG+".\nI like to spend my time "+verb_IngH+" or "+verb_IngI+".\nWhen parents go on a vacation, they spend their time eating three \
-"+plurNounJ+" a day,\n and fathers play golf, and mothers sit around "+verb_IngK+".\nLast summer, my little brother fell in a\\an "+nounL+" \
-and got poison "+plantM+" all over his "+bodyPartN+".\nMy family is going to (the) "+placeO+", and I will practice "+verb_IngP+".\nParents \
-need vacations more than kids because parents are always very "+adjeQ+"\n and because they have to work "+numR+" hours every day all year \
-\n making enough "+plurNounS+" to pay for the vacation.")
+print("A vacation is when you take a trip to some "+adjeA+" place with your "+adjeB+" family.")
+print("Usually you go to some place that is near a\\an "+nounC+" or up a\\an "+nounD+".")
+print("A good vacation place is one where you can ride "+plurNounE+" or play "+gameF+" or go hunting for "+plurNounG+".")
+print("I like to spend my time "+verb_IngH+" or "+verb_IngI+".")
+print("When parents go on a vacation, they spend their time eating three "+plurNounJ+" a day,\
+\n  and fathers play golf, and mothers sit around "+verb_IngK+".")
+print("Last summer, my little brother fell in a\\an "+nounL+" and got poison "+plantM+" all over his "+bodyPartN+".")
+print("My family is going to (the) "+placeO+", and I will practice "+verb_IngP+".")
+print("Parents need vacations more than kids because parents are always very "+adjeQ+"\
+\n  and because they have to work "+numR+" hours every day all year \
+\n  making enough "+plurNounS+" to pay for the vacation.")

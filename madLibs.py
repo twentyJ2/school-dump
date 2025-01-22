f9b932345d6996edc1b846ba998d740ac479caa8
twentyJ2 <rxgould03@gmail.com>
2025-01-22 14:11:13 -0800

    project 1

---
+++ madLibs.py
@@ -0,0 +1,31 @@
+# Mad Libs generator; Family Vacation
+# Mad Lib reference site: https://homeonthecorner.com/tag/mad-libs/
+adjeA=input("Input an adjective: ")
+adjeB=input("Input an adjective: ")
+nounC=input("Input a noun: ")
+nounD=input("Input a noun: ")
+plurNounE=input("Input a plural noun: ")
+gameF=input("Input a game: ")
+plurNounG=input("Input a plural noun: ")
+verb_IngH=input("Input a verb ending in \'ing\': ")
+verb_IngI=input("Input a verb ending in \'ing\': ")
+plurNounJ=input("Input a plural noun: ")
+verb_IngK=input("Input a verb ending in \'ing\': ")
+nounL=input("Input a noun: ")
+plantM=input("Input a plant: ")
+bodyPartN=input("Input a body part: ")
+placeO=input("Input a place: ")
+verb_IngP=input("Input a verb ending in \'ing\': ")
+adjeQ=input("Input an adjective: ")
+numR=input("Input a number: ")
+plurNounS=input("Input a plural noun: ")
+# variable end letter dictates [n'th word entry] rather than [n'th noun] or [n'th adverb].
+# Underscore (_) represents the need to append the following letters to a used word. E.g. verb_Ing = "walking" "talking"
+
+print("A vacation is when you take a trip to some "+adjeA+" place with your "+adjeB+" family.\nUsually you go to some place that is near \
+a\\an "+nounC+" or up a\\an "+nounD+".\nA good vacation place is one where you can ride "+plurNounE+" or play "+gameF+" or go hunting for \
+"+plurNounG+".\nI like to spend my time "+verb_IngH+" or "+verb_IngI+".\nWhen parents go on a vacation, they spend their time eating three \
+"+plurNounJ+" a day,\n and fathers play golf, and mothers sit around "+verb_IngK+".\nLast summer, my little brother fell in a\\an "+nounL+" \
+and got poison "+plantM+" all over his "+bodyPartN+".\nMy family is going to (the) "+placeO+", and I will practice "+verb_IngP+".\nParents \
+need vacations more than kids because parents are always very "+adjeQ+"\n and because they have to work "+numR+" hours every day all year \
+\n making enough "+plurNounS+" to pay for the vacation.")

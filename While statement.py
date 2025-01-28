f3fd87c04c79fcdce10a2e9cee7353caf0545763
twentyJ2 <rxgould03@gmail.com>
2025-01-28 11:47:21 -0800

    for/break/continue in notes, small in-class assignment

--- While statement.py
+++ While statement.py
@@ -3,3 +3,10 @@ increment=-1
 while increment<10:
     increment+=1
     print(increment)
+    
+for num in range(50):
+    if num%2==0:
+        continue
+    if num>=15:
+        break
+    print(num)
--- generalnotes.py
+++ generalnotes.py
@@ -13,6 +13,30 @@ while overarchIncrements>0:
     overarchIncrements-=1
 turtle.done()
 
+for num in [0,1,2,3]:
+    print(num)
+    
+for gog in range(4):
+    print(gog)
+    
+for wow in range(90,100):
+    print(wow)
+    
+for examp in range(90,100,2):
+    print(examp)
+    
+for anex in range(10,0,-1):
+    print(anex)
+    
+for gunk in range(10):
+    if (gunk==3):
+        break
+    print(gunk)
+    
+for num in range(10):
+    if num % 2 ==0:
+        continue
+    print(num)
 '''print(1,2,3,sep="canbeanything")
 print(2, end="alsowhatever")
 print(3,end="!!\n")'''

0ee11477022e1c68def098fe53c1b1c1c8f0d8fd
twentyJ2 <rxgould03@gmail.com>
2025-02-09 15:33:57 -0800

    Fixup

--- numberguess.py
+++ numberguess.py
@@ -5,13 +5,15 @@ mysteryNum=random.randint(1,10)
 print(mysteryNum)
 victoryCheck=False
 guessCount=0
+guessMin=1
+guessMax=10
 print("Welcome to the number guessing game.\nYou will have 3 chances to guess a mystery number between 1-10.\n\n")
 
 for i in range(3,0,-1):
     print(f"You have {i} guess(es) left.")
-    guess=input("Enter an integer between 1-10 --> ")
-    while not guess.isnumeric() or int(guess)>10 or int(guess)<1:
-        print(f"{guess} is invalid.\n\nInput exclusively one whole number ranging from 1-10. --> ")
+    guess=input(f"Enter an integer between {guessMin}-{guessMax} --> ")
+    while not guess.isnumeric() or int(guess)>guessMax or int(guess)<guessMin:
+        print(f"{guess} is invalid...\n\nInput exclusively one whole number ranging from {guessMin}-{guessMax}. --> ")
         guess=input()
     guessCount+=1
     if int(guess)==mysteryNum:
@@ -19,8 +21,10 @@ for i in range(3,0,-1):
             break
     elif int(guess)>mysteryNum:
         print("Guessed too high!\n\n")
+        guessMax=int(guess)-1
     else:
         print("Guessed too low!\n\n")
+        guessMin=int(guess)+1
 
 if victoryCheck:
     print(f"Congrats! You guessed the correct number.\nGuesses taken to complete: {guessCount}")
